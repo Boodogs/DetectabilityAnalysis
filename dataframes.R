8200e2b9c61abfe70384d72b931f419bd73bf518
Boodogs <lj.mcleod@gmail.com>
2016-09-04 01:46:04 -0600

    Cleaned up dataframes. This is the non-automated version

--- dataframes.R
+++ dataframes.R
@@ -13,32 +13,17 @@ for(i in sites){
   fulldf[fulldf$site.key == i, 'TEMP_RANK'] <- 1:length(fulldf[fulldf$site.key == i, 'TEMP_RANK'])
 }
 
+years <- levels(fulldf$sYEAR)
+
 require(reshape)
 df2013 <- subset(fulldf, sYEAR == 2013)
-fulldf2013 <- cast(df2013, SiteStnName ~ TEMP_RANK, value = fulldf$Detection)
+df2014 <- subset(fulldf, sYEAR == 2014)
+df2015 <- subset(fulldf, sYEAR == 2015)
 
-###require(dplyr)
+#data.by.station <- split(df2013, df2013$SiteStnName)
 
-#test <- fulldf %>%
-#    group_by(SiteStnName) %>%
-#    group_by(sYEAR) %>%
-#    slice(which.min(TEMP_RANK))
+write.csv(df2013, "df2013.csv")
+write.csv(df2014, "df2014.csv")
+write.csv(df2015, "df2015.csv")
 
-    
-
-##test <- aggregate(TEMP_RANK ~ SiteStnName + sYEAR, fulldf, function(x) min(x))
-
-yeardfs <- split(fulldf, f = fulldf$sYEAR)
-str(yeardfs) 
-
-df2013 <- yeardfs$`2013`
-df2014 <- yeardfs$`2014`
-df2015 <- yeardfs$`2015`
-
-sites2013 <- length(df2013$SiteStnName)
-visits2013 <- max(aggregate((df2013), list(df2013$SiteStnName), length)$value)
-
-data.wide(
-)
-
-sapply(yeardfs, function(x) mean(x$JulianDate))
+### Rearrange the year csv's to produce detection / date / hour wide format csvs
